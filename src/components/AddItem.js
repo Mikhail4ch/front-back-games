75b06e28fe08fd8d7f8a3cdcdca0b34bb40df3a8
Mikhail4ch <mihamakarevi426@gmail.com>
2024-01-29 19:27:23 +0300

    new

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,27 +1,45 @@
-const ItemDisplay = ({items}) => {
+import { useState } from "react"
+
+const AddItem = ({ handleAddName, handleAddPrice, handleAddDescription, handleAddCategory_id}) => {
+    const [itemName, setItemName] = useState("");
+    const [itemPrice, setItemPrice] = useState("");
+    const [itemDescription, setItemDescription] = useState("");
+    const [itemCategory_id, setItemCategory_id] = useState("");
+
+    const handleName = (event) => {
+        setItemName(event.target.value)
+    }
+
+    const handlePrice = (event) => {
+        setItemPrice(event.target.value)
+    }
+
+    const handleDescription = (event) => {
+        setItemDescription(event.target.value)
+    }
+
+    const handleCategory_id = (event) => {
+        setItemCategory_id(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleAddName(itemName)
+        handleAddPrice(itemPrice)
+        handleAddDescription(itemDescription)
+        handleAddCategory_id(itemCategory_id)
+    }
 
     return (
         <div>
-            <h2>List of Items</h2>
-            <table>
-                <th>
-                    <td>Name</td>
-                    <td>Price</td>
-                    <td>Description</td>
-                    <td>Category_id</td>
-                </th>
-                {items && 
-                    items.map((item) => {
-                        return<tr key={item.id}>
-                            <td>{item.name}</td>
-                            <td>{item.price}</td>
-                            <td>{item.description}</td>
-                            <td>{item.category_id}</td>
-                        </tr>
-                    })
-                }
-            </table>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Item Name" onChange={handleName}/>
+                <input type="text" name="name" placeholder="Item Price" onChange={handlePrice}/>
+                <input type="text" name="name" placeholder="Item Description" onChange={handleDescription}/>
+                <input type="text" name="name" placeholder="Item Category_id" onChange={handleCategory_id}/>
+                <button type="submit">Add</button>
+            </form>
         </div>
     )
 }
-export default ItemDisplay
+export default AddItem
